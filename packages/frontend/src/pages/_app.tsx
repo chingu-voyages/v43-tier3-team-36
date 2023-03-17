e436952c5f18abd17008ee30538521912cd62db7
Tarwat <t.uddin9121@gmail.com>
2023-03-17 18:26:58 +0000

    chore(packages): added frontend package
    Setup Next.js app with typescript

---
+++ packages/frontend/src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { ReactElement } from 'react';
+import React from 'react';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => React.ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return getLayout(<Component {...pageProps} />);
+};
+
+export default App;
---
+++ packages/frontend/src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+const Document: React.FC = () => (
+  <Html lang="en">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
