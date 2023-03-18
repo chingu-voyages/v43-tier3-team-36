7c21d81e3553ff4b949c67f9197d78bf7ee51f56
Carl Bebli <beblicarl.cb@gmail.com>
2023-03-18 15:05:32 +0000

    isLoggedIn middleware to check if a user is logged in

---
+++ packages/backend/src/middleware/isLoggedIn.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+import { User, UserPartial } from "../schemas";
+
+export const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(401).json({ message: "Unauthorized"});
+  }
+};
