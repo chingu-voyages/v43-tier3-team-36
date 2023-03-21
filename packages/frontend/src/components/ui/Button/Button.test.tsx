c9a31454a062598a9d4a2d24ea40d849a3788404
Tarwat <t.uddin9121@gmail.com>
2023-03-21 14:31:26 +0000

    feat(components): added Button component
    ui component to be used in places where native html button element instead

---
+++ packages/frontend/src/components/ui/Button/Button.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from 'test-utils';
+import userEvent from '@testing-library/user-event';
+
+import { Button } from './Button';
+
+describe('<Button />', () => {
+  describe('Snapshots', () => {
+    it('should match snapshot', () => {
+      const { container } = render(<Button />);
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match snapshot with outlined variant', () => {
+      const { container } = render(<Button variant="outlined">Foo</Button>);
+      expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('value prop', () => {
+    it('should display value as Button children', () => {
+      render(<Button>Foo</Button>);
+      expect(screen.getByRole('button')).toHaveTextContent('Foo');
+    });
+  });
+
+  describe('onClick prop', () => {
+    it('should call onClick function when Button is clicked', async () => {
+      const mockFn = jest.fn();
+      render(<Button onClick={mockFn}>Foo</Button>);
+      await userEvent.click(screen.getByRole('button'));
+      expect(mockFn).toHaveBeenCalled();
+    });
+  });
+});
