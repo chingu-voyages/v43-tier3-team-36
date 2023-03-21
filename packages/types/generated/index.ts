9e733b31c727555e32ac12ba6556e612cc636e8b
Carl Bebli <beblicarl.cb@gmail.com>
2023-03-21 10:34:21 +0000

    chore(backend setup): general update

--- packages/types/generated/index.ts
+++ packages/types/generated/index.ts
@@ -5,33 +5,39 @@ import type { Prisma } from '@prisma/client';
 // HELPER FUNCTIONS
 /////////////////////////////////////////
 
-
 /////////////////////////////////////////
 // ENUMS
 /////////////////////////////////////////
 
-export const ComicScalarFieldEnumSchema = z.enum(['id','userId']);
+export const CollectionItemScalarFieldEnumSchema = z.enum([
+  'id',
+  'collectionId',
+  'comicId',
+  'userId',
+]);
 
-export const HistoryItemScalarFieldEnumSchema = z.enum(['tradeType','comidId','historyUserId']);
+export const CollectionScalarFieldEnumSchema = z.enum(['id', 'userId']);
 
-export const HistoryScalarFieldEnumSchema = z.enum(['userId']);
+export const QueryModeSchema = z.enum(['default', 'insensitive']);
 
-export const QueryModeSchema = z.enum(['default','insensitive']);
+export const SortOrderSchema = z.enum(['asc', 'desc']);
 
-export const SortOrderSchema = z.enum(['asc','desc']);
+export const TransactionIsolationLevelSchema = z.enum([
+  'ReadUncommitted',
+  'ReadCommitted',
+  'RepeatableRead',
+  'Serializable',
+]);
 
-export const TransactionIsolationLevelSchema = z.enum(['ReadUncommitted','ReadCommitted','RepeatableRead','Serializable']);
-
-export const TransactionScalarFieldEnumSchema = z.enum(['id','userId','accepted','type','transactionsId']);
-
-export const TransactionsScalarFieldEnumSchema = z.enum(['id','userId']);
-
-export const UserScalarFieldEnumSchema = z.enum(['id','firstName','lastName','email','password','username','profileImage']);
-
-export const TradeTypeSchema = z.enum(['EXCHANGE','BUY','SELL']);
-
-export type TradeTypeType = `${z.infer<typeof TradeTypeSchema>}`
-
+export const UserScalarFieldEnumSchema = z.enum([
+  'id',
+  'firstName',
+  'lastName',
+  'email',
+  'password',
+  'username',
+  'profileImage',
+]);
 /////////////////////////////////////////
 // MODELS
 /////////////////////////////////////////
@@ -48,64 +54,30 @@ export const UserSchema = z.object({
   password: z.string(),
   username: z.string(),
   profileImage: z.string().nullable(),
-})
+});
 
-export type User = z.infer<typeof UserSchema>
+export type User = z.infer<typeof UserSchema>;
 
 /////////////////////////////////////////
-// COMIC SCHEMA
+// COLLECTION SCHEMA
 /////////////////////////////////////////
 
-export const ComicSchema = z.object({
-  id: z.string(),
+export const CollectionSchema = z.object({
+  id: z.number().int(),
   userId: z.number().int().nullable(),
-})
+});
 
-export type Comic = z.infer<typeof ComicSchema>
-
-/////////////////////////////////////////
-// HISTORY ITEM SCHEMA
-/////////////////////////////////////////
-
-export const HistoryItemSchema = z.object({
-  tradeType: TradeTypeSchema,
-  comidId: z.string(),
-  historyUserId: z.number().int().nullable(),
-})
-
-export type HistoryItem = z.infer<typeof HistoryItemSchema>
+export type Collection = z.infer<typeof CollectionSchema>;
 
 /////////////////////////////////////////
-// HISTORY SCHEMA
+// COLLECTION ITEM SCHEMA
 /////////////////////////////////////////
 
-export const HistorySchema = z.object({
+export const CollectionItemSchema = z.object({
+  id: z.number().int(),
+  collectionId: z.number().int().nullable(),
+  comicId: z.number().int(),
   userId: z.number().int(),
-})
+});
 
-export type History = z.infer<typeof HistorySchema>
-
-/////////////////////////////////////////
-// TRANSACTION SCHEMA
-/////////////////////////////////////////
-
-export const TransactionSchema = z.object({
-  type: TradeTypeSchema,
-  id: z.string(),
-  userId: z.number().int(),
-  accepted: z.boolean(),
-  transactionsId: z.string().nullable(),
-})
-
-export type Transaction = z.infer<typeof TransactionSchema>
-
-/////////////////////////////////////////
-// TRANSACTIONS SCHEMA
-/////////////////////////////////////////
-
-export const TransactionsSchema = z.object({
-  id: z.string(),
-  userId: z.number().int(),
-})
-
-export type Transactions = z.infer<typeof TransactionsSchema>
+export type CollectionItem = z.infer<typeof CollectionItemSchema>;
