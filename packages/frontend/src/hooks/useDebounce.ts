1dd497ea955d74faf8eb539724dbc883ec4654b6
Tarwat <t.uddin9121@gmail.com>
2023-03-22 17:26:32 +0000

    style(use-debounce): added jsdocs and other minor formatting improvements

--- packages/frontend/src/hooks/useDebounce.ts
+++ packages/frontend/src/hooks/useDebounce.ts
@@ -1,16 +1,25 @@
 import { useEffect, useState } from 'react';
 
+/**
+ * @description Takes a value and "debounces" it each time with a delay
+ * until the value is not changed again
+ * @param value - stateful value to be set
+ * @param delay - interval time (ms) between "debounces"
+ * @returns value
+ */
 function useDebounce<T>(value: T, delay: number): T {
-  // State and setters for debounced value
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
+
     return () => {
       clearTimeout(handler);
     };
   }, [value, delay]);
+
   return debouncedValue;
 }
 
