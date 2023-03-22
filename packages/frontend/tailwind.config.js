940ee847d55983a8c37638ec7912d6e4863eba3c
Pearl Akpan <pearlakpan115@gmail.com>
2023-03-22 13:08:54 +0100

    Chore/issue 45/tailwind globals (#46)
    * chore(fonts): import dmsans font and set base styles for font-family
    
    * chore(tailwind-themes): extend fontFamily utility classes to include dmsans

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dmsans: 'DM Sans, sans-serif',
+      },
+    },
   },
   plugins: [],
 };
