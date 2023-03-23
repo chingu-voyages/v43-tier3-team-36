ff22876b355955633a9db9a899813a71d0b5843e
fmtabbara <fmtabbara@hotmail.co.uk>
2023-03-23 23:01:34 +0000

    update image config

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -7,10 +7,16 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '*',
+        protocol: 'http',
+        hostname: 'i.annihil.us',
         port: '',
-        pathname: '/*',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
