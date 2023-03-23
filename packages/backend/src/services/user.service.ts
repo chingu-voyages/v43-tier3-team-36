d1fec261c2b53d8ddb60613db5ddab0913bddfca
fmtabbara <fmtabbara@hotmail.co.uk>
2023-03-23 19:31:11 +0000

    await prisma updates

--- packages/backend/src/services/user.service.ts
+++ packages/backend/src/services/user.service.ts
@@ -5,39 +5,46 @@ import {
 } from '@marvel-collector/types/generated';
 import prisma from '../database/PrismaClient';
 
-export const createUser = (payload: UserOptionalDefaults) => {
-  try {
-    prisma.user.create({
-      data: { ...payload },
-    });
-  } catch (e) {
-    console.log(e);
-  }
+export const createUser = async (payload: UserOptionalDefaults) => {
+  const user = await prisma.user.create({
+    data: payload,
+  });
+
+  return user;
 };
 
-export const findUserByUsername = (payload: UserPartial) =>
-  prisma.user.findUnique({
+export const findUserByUsername = async (payload: UserPartial) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: payload.username,
+    },
+  });
+  return user;
+};
+
+export const findUser = async (payload: UserPartial) => {
+  const user = await prisma.user.findUnique({
     where: {
       username: payload.username,
     },
   });
 
-export const findUser = async (payload: UserPartial) =>
-  prisma.user.findUnique({
-    where: {
-      username: payload.username,
-    },
-  });
-export const findUserByEmail = async (payload: UserPartial) =>
-  prisma.user.findUnique({
+  return user;
+};
+export const findUserByEmail = async (payload: UserPartial) => {
+  await prisma.user.findUnique({
     where: {
       username: payload.email,
     },
   });
+};
 
-export const findUserById = async (payload: UserPartial) =>
-  prisma.user.findUnique({
+export const findUserById = async (payload: UserPartial) => {
+  const user = await prisma.user.findUnique({
     where: {
       id: payload.id,
     },
   });
+
+  return user;
+};
