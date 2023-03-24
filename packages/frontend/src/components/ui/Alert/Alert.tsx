3b1473177d08c228182348d750ec7328d6d79bd5
pearlsky <pearlakpan115@gmail.com>
2023-03-24 23:47:00 +0100

    feat: write alert ui component

---
+++ packages/frontend/src/components/ui/Alert/Alert.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { UseAlertStore } from '@/store/store';
+
+export interface IAlert {
+  type: string;
+  message: string;
+}
+
+export type AlertProps = {
+  type: string;
+  message: string;
+  icon?: React.ReactNode;
+};
+
+type AlertVariants = {
+  [type: string]: string;
+  error: string;
+  success: string;
+};
+
+const variants: AlertVariants = {
+  error: 'border-red-700 bg-red-200',
+  success: 'border-green-700 bg-green-200',
+};
+
+export const Alert = ({ type, message, icon }: AlertProps) => (
+  <section className="absolute top-8 left-0 w-full flex justify-center animate-slide-in">
+    {type && message ? (
+      <article
+        className={`${variants[type]} flex items-center px-3 py-2 rounded-lg border-2 absolute 3em`}
+      >
+        {icon}
+        {type === 'error' && (
+          <p className="text-sm">
+            <strong className="font-bold mr-1.5">Error!</strong>
+            <small className="text-sm">{message}</small>
+          </p>
+        )}
+
+        {type === 'success' && (
+          <p className="text-sm">
+            <strong className="font-bold mr-1.5">Success!</strong>
+            <small className="text-sm">{message}</small>
+          </p>
+        )}
+      </article>
+    ) : (
+      ''
+    )}
+  </section>
+);
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const AlertLayout: React.FC<Props> = ({ children }) => {
+  const alert = UseAlertStore((state: any) => state.alert);
+
+  return (
+    <section className="relative w-full min-h-screen">
+      <Alert type={alert.type} message={alert.message} />
+      {children}
+    </section>
+  );
+};
