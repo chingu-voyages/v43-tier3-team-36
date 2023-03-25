b86c07ab2113385ae284e2ccaff06d7344bc9b42
Carl Bebli <beblicarl.cb@gmail.com>
2023-03-25 22:59:50 +0000

    feat(collection service): created a new collection service

--- packages/backend/src/services/collection.service.ts
+++ packages/backend/src/services/collection.service.ts
@@ -1,48 +1,32 @@
-import {
-  Collection,
-  CollectionPartial,
-  CollectionOptionalDefaultsSchema,
-  CollectionOptionalDefaults,
-} from '@marvel-collector/types/generated/modelSchema';
-import { Prisma } from '@prisma/client';
+/* eslint-disable max-len */
 import prisma from '../database/PrismaClient';
 
 export const findUniqueId = async (userId: string) => prisma.user.findUnique({
-  where: {
-    id: userId,
-  },
+  where: { id: userId },
 });
 
-export const findUserCollection = async (userId: string) => prisma.collection.findUnique({
-  where: {
-    id: userId,
-  },
-});
-
-export const createUserCollection = async (userId: string) => prisma.collection.create({
-  data: {
-    user: { connect: { id: userId } },
-  },
-});
-export const connectCollectionItems = async (id: string) => prisma.collectionItem.findMany({
-  where: {
-    collectionId: id,
-  },
-});
-
-export const findUserWithCollection = async (userId: string) => prisma.collection.findUnique({
-  where: { userId },
-  include: { collectionItems: true },
-});
-
-export const findCollectionWithUser = async (collectionId: string) => prisma.collection.findUnique({
-  where: { id: collectionId },
-  include: { user: true },
-});
-
-export const findExistingComic = async (
+export const createCollectionItem = async (
+  userId: string,
   comicId: string,
-  collectionId: string,
+  title: string,
+  imageUrl: string,
+) => prisma.collectionItem.create({
+  data: {
+    comicId,
+    title,
+    imageUrl,
+    User: {
+      connect: { id: userId },
+    },
+  },
+});
+
+export const existingComicInCollection = async (
+  comicId: string,
+  userId: string,
 ) => prisma.collectionItem.findFirst({
-  where: { comicId, collectionId },
+  where: {
+    comicId,
+    userId,
+  },
 });
