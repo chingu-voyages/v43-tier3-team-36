8e27f8509bfe6c37d2c24bf27c6657e915cab5ea
Carl Bebli <beblicarl.cb@gmail.com>
2023-03-25 13:01:17 +0000

    feat(collection route): imported the controllers to use as routes

--- packages/backend/src/routes/v1/collection.route.ts
+++ packages/backend/src/routes/v1/collection.route.ts
@@ -1,12 +1,19 @@
+import { CollectionSchema } from '@marvel-collector/types/generated';
 import { Router } from 'express';
 import {
-  assignComicToCollection,
+  assignCollectionItems,
+  createCollection,
 } from '../../controllers/collection.controller';
-import {
-  isLoggedIn,
-} from '../../middleware';
+import { isLoggedIn, validateSchema } from '../../middleware';
+import { AssignComicSchema } from '../../utils/customValidation';
 
 const router = Router();
-router.post('/user/collection', isLoggedIn, assignComicToCollection);
+router.post('/user/collection', isLoggedIn, createCollection);
+router.post(
+  '/user/collection/:collectionId/comic',
+  isLoggedIn,
+  validateSchema(AssignComicSchema),
+  assignCollectionItems,
+);
 
 export default router;
