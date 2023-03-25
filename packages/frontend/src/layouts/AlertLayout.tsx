3de2f66010f7da3c5da26f4749c40069aa39f539
pearlsky <pearlakpan115@gmail.com>
2023-03-25 22:42:15 +0100

    refactor: write app w layout for alert component

---
+++ packages/frontend/src/layouts/AlertLayout.tsx
@@ -0,0 +1,16 @@
+// import { Alert } from '@/components/ui/Alert';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+//   const alert = UseAlertStore((state: any) => state.alert);
+
+const AlertLayout: React.FC<Props> = ({ children }) => (
+  <section className="relative w-full min-h-screen">
+    {/* <Alert type={alert.type} message={alert.message} /> */}
+    {children}
+  </section>
+);
+
+export default AlertLayout;
