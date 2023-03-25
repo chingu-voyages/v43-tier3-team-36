f517adf50ed4c8f327a1e05b01b282a624725c53
Carl Bebli <beblicarl.cb@gmail.com>
2023-03-25 13:09:34 +0000

    feat(Types): new types generated for the user model

--- packages/types/generated/inputTypeSchemas/index.ts
+++ packages/types/generated/inputTypeSchemas/index.ts
@@ -1,8 +1,6 @@
+export { CollectionItemScalarFieldEnumSchema } from './CollectionItemScalarFieldEnumSchema';
+export { CollectionScalarFieldEnumSchema } from './CollectionScalarFieldEnumSchema';
 export { QueryModeSchema } from './QueryModeSchema';
 export { SortOrderSchema } from './SortOrderSchema';
 export { TransactionIsolationLevelSchema } from './TransactionIsolationLevelSchema';
 export { UserScalarFieldEnumSchema } from './UserScalarFieldEnumSchema';
-export { transformJsonNull } from './transformJsonNull';
-export { NullableJsonValue } from './NullableJsonValue';
-export { InputJsonValue } from './InputJsonValue';
-export { JsonValue } from './JsonValue';
