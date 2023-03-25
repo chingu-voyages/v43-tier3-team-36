cfc243f49f1741f5f053cddf309aa355d340bf0f
Tarwat <t.uddin9121@gmail.com>
2023-03-25 15:32:07 +0000

    feat(modal): improved types, added minor refactors and others changes

--- packages/frontend/src/components/ui/Modal/Modal.tsx
+++ packages/frontend/src/components/ui/Modal/Modal.tsx
@@ -1,82 +1,85 @@
-import { Dialog, Transition } from '@headlessui/react';
-import React, { Fragment, ReactNode } from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import { Dialog, Transition, type DialogProps } from '@headlessui/react';
+import clsx from 'clsx';
+import type { ForwardRefExoticComponent, RefAttributes } from 'react';
+import { forwardRef, Fragment } from 'react';
 
-type TModalProps = {
+export interface ModalProps {
+  children?: React.ReactNode;
   title: string;
   description?: string;
-  show: boolean;
-  onClose: () => void;
-  Actions?: ReactNode;
-  children?: ReactNode;
-};
+  actions: React.ElementType[];
+}
 
-export const Modal = ({
-  title,
-  description,
-  show,
-  onClose,
-  Actions,
-  children,
-}: TModalProps) => (
-  <Transition.Root show={show} as={Fragment}>
-    <Dialog as="div" className="relative z-10" onClose={onClose}>
-      <Transition.Child
-        as={Fragment}
-        enter="ease-out duration-300"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="ease-in duration-200"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-      </Transition.Child>
+export const Modal: ForwardRefExoticComponent<
+ModalProps & DialogProps<'div'> & RefAttributes<HTMLDivElement>
+> = forwardRef(
+  (
+    {
+      children, title, description, open, actions, className, ...rest
+    },
+    ref,
+  ) => (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog ref={ref} className="relative z-10" {...rest}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
 
-      <div className="fixed inset-0 z-10 overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          >
-            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-base font-semibold leading-6 text-gray-900"
-                    >
-                      {title}
-                    </Dialog.Title>
-                    {description && (
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Are you sure you want to deactivate your account? All
-                          of your data will be permanently removed. This action
-                          cannot be undone.
-                        </p>
-                      </div>
-                    )}
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel
+                className={clsx(
+                  'relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg',
+                  className,
+                )}
+              >
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900"
+                      >
+                        {title}
+                      </Dialog.Title>
+                      {description ? (
+                        <div className="mt-2">
+                          <p className="text-sm text-gray-500">{description}</p>
+                        </div>
+                      ) : null}
+                    </div>
                   </div>
                 </div>
-              </div>
-              {children && <div>{children}</div>}
-              {Actions && (
+                {children ? <div>{children}</div> : null}
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                  {Actions}
+                  {actions.map((Component, idx) => (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <Component key={idx} />
+                  ))}
                 </div>
-              )}
-            </Dialog.Panel>
-          </Transition.Child>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
         </div>
-      </div>
-    </Dialog>
-  </Transition.Root>
+      </Dialog>
+    </Transition.Root>
+  ),
 );
-
-export default Modal;
--- packages/frontend/src/components/ui/index.ts
+++ packages/frontend/src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export * from './Button';
 export * from './Avatar';
 export * from './Input';
+export * from './Modal';
