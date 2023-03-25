62106b6f4edce4e644f9039bd372fbd98b937b45
Carl Bebli <beblicarl.cb@gmail.com>
2023-03-25 17:20:44 +0000

    feat(collection route): commented out the createCollection endpoint

--- packages/backend/src/routes/v1/collection.route.ts
+++ packages/backend/src/routes/v1/collection.route.ts
@@ -2,13 +2,13 @@ import { CollectionSchema } from '@marvel-collector/types/generated';
 import { Router } from 'express';
 import {
   assignCollectionItems,
-  createCollection,
+  // createCollection,
 } from '../../controllers/collection.controller';
 import { isLoggedIn, validateSchema } from '../../middleware';
 import { AssignComicSchema } from '../../utils/customValidation';
 
 const router = Router();
-router.post('/user/collection', isLoggedIn, createCollection);
+// router.post('/user/collection', isLoggedIn, createCollection);
 router.post(
   '/user/collection/:collectionId/comic',
   isLoggedIn,
