8a674a191a378ae346e7e7bdee04adbad027f63a
Carl Bebli <beblicarl.cb@gmail.com>
2023-03-25 13:10:28 +0000

    feat(Types): new types generated for the user model

--- packages/types/generated/modelSchema/UserSchema.ts
+++ packages/types/generated/modelSchema/UserSchema.ts
@@ -1,5 +1,4 @@
 import { z } from 'zod';
-import { InputJsonValue } from "../inputTypeSchemas/InputJsonValue"
 
 /////////////////////////////////////////
 // USER SCHEMA
@@ -13,25 +12,27 @@ export const UserSchema = z.object({
   password: z.string(),
   username: z.string(),
   profileImage: z.string().nullable(),
-  collection: InputJsonValue.array(),
-})
+  location: z.string().nullable(),
+});
 
-export type User = z.infer<typeof UserSchema>
+export type User = z.infer<typeof UserSchema>;
 
 // USER PARTIAL SCHEMA
 //------------------------------------------------------
 
-export const UserPartialSchema = UserSchema.partial()
+export const UserPartialSchema = UserSchema.partial();
 
-export type UserPartial = z.infer<typeof UserPartialSchema>
+export type UserPartial = z.infer<typeof UserPartialSchema>;
 
 // USER OPTIONAL DEFAULTS SCHEMA
 //------------------------------------------------------
 
-export const UserOptionalDefaultsSchema = UserSchema.merge(z.object({
-  id: z.string().uuid().optional(),
-}))
+export const UserOptionalDefaultsSchema = UserSchema.merge(
+  z.object({
+    id: z.string().uuid().optional(),
+  }),
+);
 
-export type UserOptionalDefaults = z.infer<typeof UserOptionalDefaultsSchema>
+export type UserOptionalDefaults = z.infer<typeof UserOptionalDefaultsSchema>;
 
 export default UserSchema;
