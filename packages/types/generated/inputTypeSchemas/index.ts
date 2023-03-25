2579084ed70174876f6b7eb896c0076af077979d
Carl Bebli <beblicarl.cb@gmail.com>
2023-03-25 23:06:10 +0000

    feat(user types): generated new user types

--- packages/types/generated/inputTypeSchemas/index.ts
+++ packages/types/generated/inputTypeSchemas/index.ts
@@ -1,5 +1,4 @@
 export { CollectionItemScalarFieldEnumSchema } from './CollectionItemScalarFieldEnumSchema';
-export { CollectionScalarFieldEnumSchema } from './CollectionScalarFieldEnumSchema';
 export { QueryModeSchema } from './QueryModeSchema';
 export { SortOrderSchema } from './SortOrderSchema';
 export { TransactionIsolationLevelSchema } from './TransactionIsolationLevelSchema';
