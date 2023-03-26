a55d60192fb60bdfb5b4b82b513558f0e2dce060
Danthesage01 <ayenibabajidedaniel@gmail.com>
2023-03-26 18:06:27 +0100

    feat: add activities list component

---
+++ packages/frontend/src/components/ui/Activities/Activities.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// export type TPActivitiesProps = [];
+
+import { Activity } from '../Activity';
+
+const Activities = () => (
+  <div className="flex flex-col gap-8">
+    <Activity
+      img=" https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      name="Philips"
+      comicTitle="Wolverine"
+    />
+    <Activity
+      img=" https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      name="Philips"
+      comicTitle="Wolverine"
+    />
+    <Activity
+      img=" https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      name="Philips"
+      comicTitle="Wolverine"
+    />
+  </div>
+);
+export default Activities;
