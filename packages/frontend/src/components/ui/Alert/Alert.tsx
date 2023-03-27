fb7ce0dad201f9982f07f34ad6e581710c5988a2
pearlsky <pearlakpan115@gmail.com>
2023-03-27 15:56:10 +0100

    refactor: update conditional rendering to avoid duplication

--- packages/frontend/src/components/ui/Alert/Alert.tsx
+++ packages/frontend/src/components/ui/Alert/Alert.tsx
@@ -22,27 +22,17 @@ const variants: AlertVariants = {
 
 export const Alert = ({ type, message, icon }: AlertProps) => (
   <section className="absolute top-8 left-0 w-full flex justify-center animate-slide-in">
-    {type && message ? (
-      <article
-        className={`${variants[type]} flex items-center px-3 py-2 rounded-lg border-2 absolute 3em`}
-      >
-        {icon}
-        {type === 'error' && (
-          <p className="text-sm">
-            <strong className="font-bold mr-1.5">Error!</strong>
-            <small className="text-sm">{message}</small>
-          </p>
-        )}
-
-        {type === 'success' && (
-          <p className="text-sm">
-            <strong className="font-bold mr-1.5">Success!</strong>
-            <small className="text-sm">{message}</small>
-          </p>
-        )}
-      </article>
-    ) : (
-      ''
-    )}
+    <article
+      className={`${variants[type]} flex items-center px-3 py-2 rounded-lg border-2 absolute 3em`}
+    >
+      {icon}
+      <p className="text-sm">
+        <strong className="mr-1.5 capitalize font-bold">
+          {type}
+          !
+        </strong>
+        <small className="text-sm">{message}</small>
+      </p>
+    </article>
   </section>
 );
