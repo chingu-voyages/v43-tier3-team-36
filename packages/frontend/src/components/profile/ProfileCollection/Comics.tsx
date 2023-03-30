a3ca5ab566f333a483b1a086275c285ba2b82812
Tarwat Uddin <91470221+TazDeCoder@users.noreply.github.com>
2023-03-30 22:43:59 +0000

    fix(profile-collection): added changes to fetch comics from user corr… (#87)
    * fix(profile-collection): added changes to fetch comics from user correctly
    
    * refactor(profile-collection): removed arrow function callback

--- packages/frontend/src/components/profile/ProfileCollection/Comics.tsx
+++ packages/frontend/src/components/profile/ProfileCollection/Comics.tsx
@@ -3,22 +3,23 @@ import { useQuery } from '@tanstack/react-query';
 import clsx from 'clsx';
 import { Trash2 } from 'lucide-react';
 
-import { getCurrentUserDetails } from '@/api';
+import { getComicBookCollector } from '@/api';
 import { createImageUrl } from '@/utils';
 
 const COMIC_FALLBACK = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5876fe74-9e55-493f-a3e0-e95076ef3962/dfld4s6-678156d5-357d-4f01-83c8-39c555889984.jpg/v1/fill/w_1024,h_742,q_75,strp/comic_placeholder_1_by_oceanside7_dfld4s6-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzQyIiwicGF0aCI6IlwvZlwvNTg3NmZlNzQtOWU1NS00OTNmLWEzZTAtZTk1MDc2ZWYzOTYyXC9kZmxkNHM2LTY3ODE1NmQ1LTM1N2QtNGYwMS04M2M4LTM5YzU1NTg4OTk4NC5qcGciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.0wYHMk-cn4o0BLbDEDyebHIAr7lj265-7yHRRcbmSGQ';
 
 type Props = {
+  userId: string;
   isEdit: boolean;
 };
 
-const Comics: React.FC<Props> = ({ isEdit }) => {
+const Comics: React.FC<Props> = ({ userId, isEdit }) => {
   const {
-    data: userData,
+    data: comicsData,
     isLoading,
     isError,
-  } = useQuery(['user'], {
-    queryFn: () => getCurrentUserDetails(),
+  } = useQuery(['user-comics'], {
+    queryFn: () => getComicBookCollector(userId),
   });
   // const mutation = useMutation(...);
 
@@ -42,7 +43,7 @@ const Comics: React.FC<Props> = ({ isEdit }) => {
   return (
     <div className="grid grid-cols-3 lg:grid-cols-none lg:grid-flow-col justify-items-center gap-y-5 lg:gap-x-2 py-5 md:py-11 px-2 md:px-8 mt-8 mb-6 overflow-x-auto bg-neutral-100 border border-zinc-200 rounded-xl">
       {/* @ts-ignore */}
-      {userData?.comics.map(({ id, images }) => (
+      {comicsData?.comics.map(({ id, images }) => (
         <div
           key={id}
           className="relative w-28 sm:w-36 md:w-44 lg:w-56 h-36 sm:h-44 md:h-48 lg:h-64 max-w-full"
