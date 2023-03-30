39dd324c75cf6adb8e238c0dfc8e21af3beb7782
Carl Bebli <beblicarl.cb@gmail.com>
2023-03-30 08:17:02 +0000

    feat(migrations): generated new migrations

---
+++ packages/backend/prisma/migrations/20230329174425_init/migration.sql
@@ -0,0 +1,63 @@
+-- CreateEnum
+CREATE TYPE "TradeOfferType" AS ENUM ('EXCHANGE', 'SELL');
+
+-- CreateEnum
+CREATE TYPE "TradeOfferStatus" AS ENUM ('PENDING', 'ACCEPTED', 'DECLINED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "profileImage" TEXT,
+    "location" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CollectionItem" (
+    "id" TEXT NOT NULL,
+    "comicId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "userId" TEXT,
+    "tradeOfferId" TEXT,
+
+    CONSTRAINT "CollectionItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TradeOffer" (
+    "id" TEXT NOT NULL,
+    "type" "TradeOfferType" NOT NULL,
+    "status" "TradeOfferStatus" NOT NULL DEFAULT 'PENDING',
+    "createdById" TEXT NOT NULL,
+    "price" DOUBLE PRECISION,
+    "phoneNumber" TEXT,
+    "message" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "TradeOffer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CollectionItem_comicId_key" ON "CollectionItem"("comicId");
+
+-- AddForeignKey
+ALTER TABLE "CollectionItem" ADD CONSTRAINT "CollectionItem_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CollectionItem" ADD CONSTRAINT "CollectionItem_tradeOfferId_fkey" FOREIGN KEY ("tradeOfferId") REFERENCES "TradeOffer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TradeOffer" ADD CONSTRAINT "TradeOffer_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
