ad18b7b40db60aa2efd2a00d4b4e68f2399082fc
Tarwat <t.uddin9121@gmail.com>
2023-03-30 19:27:26 +0100

    fix(profile-collection): added changes to fetch comics from user correctly

--- packages/frontend/src/components/profile/ProfileCollection/ProfileCollection.tsx
+++ packages/frontend/src/components/profile/ProfileCollection/ProfileCollection.tsx
@@ -1,19 +1,39 @@
+import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
-import Actions from './Actions';
 
 import Comics from './Comics';
+import Actions from './Actions';
+import { getCurrentUserDetails } from '@/api';
 
 const ProfileComics: React.FC = () => {
+  const {
+    data: userData,
+    isLoading,
+    isError,
+  } = useQuery(['user'], {
+    queryFn: () => getCurrentUserDetails(),
+  });
   const [isEdit, setIsEdit] = useState(false);
 
   const toggleEditHandler = () => {
     setIsEdit((prevState) => !prevState);
   };
 
+  if (isLoading) {
+    // TODO: Replace with loader component
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    // TODO: Replace with alert/error component
+    return <span>Failed to retrieve user!</span>;
+  }
+
   return (
     <section>
       <h2 className="font-bold">Your Collection</h2>
-      <Comics isEdit={isEdit} />
+      {/* @ts-ignore */}
+      <Comics userId={userData.userId} isEdit={isEdit} />
       <Actions onEdit={toggleEditHandler} />
     </section>
   );
