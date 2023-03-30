dd8fe4fa24a91a44769d20b53c6deaeda11f07ca
Tarwat <t.uddin9121@gmail.com>
2023-03-30 18:45:12 +0100

    feat(profile-collection): added mutation of deletion of comics via backend service

--- packages/frontend/src/components/profile/ProfileCollection/ProfileCollection.tsx
+++ packages/frontend/src/components/profile/ProfileCollection/ProfileCollection.tsx
@@ -1,20 +1,43 @@
 import { useState } from 'react';
-import Actions from './Actions';
+import { useMutation } from '@tanstack/react-query';
 
 import Comics from './Comics';
+import Actions from './Actions';
+import UseAlertStore from '@/store/store';
+import { removeComic } from '@/api';
 
 const ProfileComics: React.FC = () => {
+  const setAlert = UseAlertStore((state: any) => state.setAlert);
   const [isEdit, setIsEdit] = useState(false);
+
+  const removeComicMutation = useMutation({
+    mutationFn: removeComic,
+    onSuccess: (data) => {
+      setAlert({ type: 'success', message: data.message });
+    },
+    onError: (err) => {
+      setAlert({
+        type: 'error',
+        message: err,
+      });
+    },
+  });
+
+  const removeComicHandler = (id: number) => {
+    removeComicMutation.mutate(id);
+  };
 
   const toggleEditHandler = () => {
     setIsEdit((prevState) => !prevState);
   };
 
+  const disableActions = removeComicMutation.isLoading;
+
   return (
     <section>
       <h2 className="font-bold">Your Collection</h2>
-      <Comics isEdit={isEdit} />
-      <Actions onEdit={toggleEditHandler} />
+      <Comics isEdit={isEdit} onRemoveComic={removeComicHandler} />
+      <Actions disabled={disableActions} onEdit={toggleEditHandler} />
     </section>
   );
 };
