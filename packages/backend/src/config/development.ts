6eeb5e7645cdd733711234cd1533069f4ed45f3e
Danthesage01 <ayenibabajidedaniel@gmail.com>
2023-03-31 18:03:55 +0100

    chore(development): add dev origin and prod origin

--- packages/backend/src/config/development.ts
+++ packages/backend/src/config/development.ts
@@ -6,4 +6,6 @@ export default {
   DATABASE_URL: process.env.DATABASE_URL,
   PORT: process.env.PORT,
   SESSION_SECRET: process.env.SESSION_SECRET,
+  DEV_ORIGIN: process.env.DEV_ORIGIN,
+  PROD_ORIGIN: process.env.PROD_ORIGIN,
 };
