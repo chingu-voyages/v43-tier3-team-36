b6279f1b4c16143bd665a0701c9812fce29d7c6e
pearlsky <pearlakpan115@gmail.com>
2023-03-31 02:43:47 +0100

    chore(component): create Navigation component; replace ExploreNavBar w Navigation component

---
+++ packages/frontend/src/components/common/Navigation/Navigation.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const Navigation: React.FC = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <nav>
+      <ul className="flex items-center gap-x-4 lg:gap-x-10">
+        <li>
+          <Link
+            className={
+              pathname === '/explore/comics' ? 'text-black' : 'text-gray-300'
+            }
+            href="./comics"
+          >
+            Comics
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={
+              pathname === '/explore/offers' ? 'text-black' : 'text-gray-300'
+            }
+            href="./offers"
+          >
+            Offers
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={
+              pathname === '/explore/collectors'
+                ? 'text-black'
+                : 'text-gray-300'
+            }
+            href="./collectors"
+          >
+            Collectors
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
