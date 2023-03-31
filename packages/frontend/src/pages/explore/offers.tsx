2d4f9d80b94119391aa4ce575be705b3da35d4f6
pearlsky <pearlakpan115@gmail.com>
2023-03-31 01:07:23 +0100

    feat(pages): write explore offers subpage

---
+++ packages/frontend/src/pages/explore/offers.tsx
@@ -0,0 +1,37 @@
+import { ReactElement, useState } from 'react';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { InputField } from '@/components/ui';
+import { NextPageWithLayout } from '../_app';
+import AuthLayout from '@/layouts/AuthLayout';
+
+const Offers: NextPageWithLayout = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const handleChange = (inputValue: string) => setSearchQuery(inputValue);
+  return (
+    <section className="my-6 md:my-8">
+      <InputField
+        placeholder="Search for a comic book offer"
+        Icon={<MagnifyingGlassIcon className="h-5 w-5 text-grey-800" />}
+        fullWidth
+        value={searchQuery}
+        onChange={handleChange}
+      />
+    </section>
+  );
+};
+
+Offers.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <AuthLayout
+      seo={{
+        title: 'Explore | Offers',
+        meta: {
+          description:
+            'View, buy and exchange comic offers from around the world',
+        },
+      }}
+    >
+      {page}
+    </AuthLayout>
+  );
+};
