1d6c1a19437066f9235af4def69a4ad7d7e0f546
Tarwat <t.uddin9121@gmail.com>
2023-04-02 02:25:22 +0100

    feat(profile-collection): added integration for displaying selected comic to trade by the user

--- packages/frontend/src/components/profile/ProfileCollection/Comics.tsx
+++ packages/frontend/src/components/profile/ProfileCollection/Comics.tsx
@@ -1,26 +1,37 @@
 import Image from 'next/image';
 import { useQuery } from '@tanstack/react-query';
 import clsx from 'clsx';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { twMerge } from 'tailwind-merge';
 import { Trash2 } from 'lucide-react';
 
 import { getComicBookCollector } from '@/api';
 import { createImageUrl } from '@/utils';
-
-const COMIC_FALLBACK = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5876fe74-9e55-493f-a3e0-e95076ef3962/dfld4s6-678156d5-357d-4f01-83c8-39c555889984.jpg/v1/fill/w_1024,h_742,q_75,strp/comic_placeholder_1_by_oceanside7_dfld4s6-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzQyIiwicGF0aCI6IlwvZlwvNTg3NmZlNzQtOWU1NS00OTNmLWEzZTAtZTk1MDc2ZWYzOTYyXC9kZmxkNHM2LTY3ODE1NmQ1LTM1N2QtNGYwMS04M2M4LTM5YzU1NTg4OTk4NC5qcGciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.0wYHMk-cn4o0BLbDEDyebHIAr7lj265-7yHRRcbmSGQ';
+import type TComicItem from '@/types/comic';
+import { COMIC_FALLBACK } from '@/data/constants';
 
 type Props = {
   userId: string;
   isEdit: boolean;
+  isPick: boolean;
   onRemoveComic: (id: number) => void;
+  onPickComic: (comic: TComicItem) => void;
 };
 
-const Comics: React.FC<Props> = ({ userId, isEdit, onRemoveComic }) => {
+const Comics: React.FC<Props> = ({
+  userId,
+  isEdit,
+  isPick,
+  onRemoveComic,
+  onPickComic,
+}) => {
   const {
-    data: comicsData,
+    data: userData,
     isLoading,
     isError,
-  } = useQuery(['user-comics'], {
+  } = useQuery(['user-comics', userId], {
     queryFn: () => getComicBookCollector(userId),
+    enabled: !!userId,
   });
 
   if (isLoading) {
@@ -36,22 +47,28 @@ const Comics: React.FC<Props> = ({ userId, isEdit, onRemoveComic }) => {
   return (
     <div className="grid grid-cols-3 px-2 py-5 mt-8 mb-6 overflow-x-auto border lg:grid-cols-none lg:grid-flow-col justify-items-center gap-y-5 lg:gap-x-2 md:py-11 md:px-8 bg-neutral-100 border-zinc-200 rounded-xl">
       {/* @ts-ignore */}
-      {comicsData?.comics.map(({ id, images }) => (
+      {userData?.collection.map((comic) => (
         <div
-          key={id}
-          className="relative max-w-full w-28 sm:w-36 md:w-44 lg:w-56 h-36 sm:h-44 md:h-48 lg:h-64"
+          key={comic.id}
+          className={twMerge(
+            clsx(
+              'relative w-28 sm:w-36 md:w-44 lg:w-56 h-36 sm:h-44 md:h-48 lg:h-64 max-w-full',
+              { 'hover:ring': isPick },
+            ),
+          )}
         >
           <Image
             className={clsx({ 'opacity-75': isEdit })}
-            src={createImageUrl(images) || COMIC_FALLBACK}
+            src={createImageUrl(comic.images) || COMIC_FALLBACK}
             alt=""
             fill
+            onClick={isPick ? () => onPickComic(comic) : undefined}
           />
           {isEdit && (
             <button
               className="absolute top-3 right-2"
               type="button"
-              onClick={() => onRemoveComic(id)}
+              onClick={() => onRemoveComic(comic.id)}
             >
               <Trash2 />
             </button>
--- packages/frontend/src/components/profile/ProfileCollection/ProfileCollection.tsx
+++ packages/frontend/src/components/profile/ProfileCollection/ProfileCollection.tsx
@@ -2,9 +2,11 @@ import { useMutation, useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 
 import Comics from './Comics';
-import Actions from './Actions';
+import CollectionActions from './CollectionActions';
 import UseAlertStore from '@/store/store';
 import { getCurrentUserDetails, removeComic } from '@/api';
+import NewTradeOffer from './NewTradeOffer';
+import TComicItem from '@/types/comic';
 
 const ProfileCollection: React.FC = () => {
   const setAlert = UseAlertStore((state) => state.setAlert);
@@ -28,13 +30,23 @@ const ProfileCollection: React.FC = () => {
     },
   });
   const [isEdit, setIsEdit] = useState(false);
+  const [isTrade, setIsTrade] = useState(false);
+  const [activeComic, setActiveComic] = useState<TComicItem | null>(null);
 
   const removeComicHandler = (id: number) => {
     removeComicMutation.mutate(id);
   };
 
+  const pickComicHandler = (pickedComic: TComicItem) => {
+    setActiveComic(pickedComic);
+  };
+
   const toggleEditHandler = () => {
     setIsEdit((prevState) => !prevState);
+  };
+
+  const toggleTradeHandler = () => {
+    setIsTrade((prevState) => !prevState);
   };
 
   if (isLoading) {
@@ -50,16 +62,32 @@ const ProfileCollection: React.FC = () => {
   const disableActions = removeComicMutation.isLoading;
 
   return (
-    <section>
-      <h2 className="font-bold">Your Collection</h2>
-      <Comics
-        // @ts-ignore
-        userId={userData.userId}
-        isEdit={isEdit}
-        onRemoveComic={removeComicHandler}
-      />
-      <Actions disabled={disableActions} onEdit={toggleEditHandler} />
-    </section>
+    <>
+      <section>
+        <h2 className="font-bold">Your Collection</h2>
+        {isTrade && <p>Please select a comic below</p>}
+        <Comics
+          // @ts-ignore
+          userId={userData?.userId}
+          isEdit={isEdit}
+          isPick={isTrade}
+          onRemoveComic={removeComicHandler}
+          onPickComic={pickComicHandler}
+        />
+        <CollectionActions
+          disabled={disableActions}
+          onEdit={toggleEditHandler}
+          onTrade={toggleTradeHandler}
+        />
+      </section>
+      {activeComic ? (
+        <NewTradeOffer
+          key={activeComic.id}
+          comic={activeComic}
+          onClose={() => setActiveComic(null)}
+        />
+      ) : null}
+    </>
   );
 };
 
