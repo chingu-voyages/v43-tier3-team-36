de7ffef3e86c7d5552b0c96a04fd42f41f30c44d
Carl Bebli <beblicarl.cb@gmail.com>
2023-04-11 17:44:37 +0000

    feat(migrations): generated new migrations for new use cases

---
+++ packages/backend/prisma/migrations/20230411171330_init/migration.sql
@@ -0,0 +1,100 @@
+-- CreateEnum
+CREATE TYPE "TradeOfferType" AS ENUM ('EXCHANGE', 'SELL');
+
+-- CreateEnum
+CREATE TYPE "TradeOfferStatus" AS ENUM ('PENDING', 'ACCEPTED', 'DECLINED');
+
+-- CreateEnum
+CREATE TYPE "TradeRequestStatus" AS ENUM ('PENDING', 'ACCEPTED', 'DECLINED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "profileImage" TEXT,
+    "location" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CollectionItem" (
+    "id" TEXT NOT NULL,
+    "comicId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "userId" TEXT,
+    "tradeOfferId" TEXT,
+
+    CONSTRAINT "CollectionItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TradeOffer" (
+    "id" TEXT NOT NULL,
+    "type" "TradeOfferType" NOT NULL,
+    "status" "TradeOfferStatus" NOT NULL DEFAULT 'PENDING',
+    "createdById" TEXT NOT NULL,
+    "price" DOUBLE PRECISION,
+    "phoneNumber" TEXT,
+    "email" TEXT,
+    "message" TEXT,
+    "wantedComicId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "TradeOffer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TradeRequest" (
+    "id" TEXT NOT NULL,
+    "receiverId" TEXT NOT NULL,
+    "tradeOfferId" TEXT NOT NULL,
+    "receiverComicId" INTEGER,
+    "status" "TradeRequestStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "TradeRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PushNotification" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "isRead" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "PushNotification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CollectionItem_comicId_userId_key" ON "CollectionItem"("comicId", "userId");
+
+-- AddForeignKey
+ALTER TABLE "CollectionItem" ADD CONSTRAINT "CollectionItem_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CollectionItem" ADD CONSTRAINT "CollectionItem_tradeOfferId_fkey" FOREIGN KEY ("tradeOfferId") REFERENCES "TradeOffer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TradeOffer" ADD CONSTRAINT "TradeOffer_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TradeRequest" ADD CONSTRAINT "TradeRequest_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TradeRequest" ADD CONSTRAINT "TradeRequest_tradeOfferId_fkey" FOREIGN KEY ("tradeOfferId") REFERENCES "TradeOffer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PushNotification" ADD CONSTRAINT "PushNotification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
