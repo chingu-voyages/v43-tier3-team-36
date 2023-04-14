78df1cb53be5eb018cb6694d882459382a911cb0
pearlsky <pearlakpan115@gmail.com>
2023-04-14 23:39:45 +0100

    fix(prisma): fix dev server startup error by runnign prisma generate

--- packages/types/generated/inputTypeSchemas/TradeRequestStatusSchema.ts
+++ packages/types/generated/inputTypeSchemas/TradeRequestStatusSchema.ts
@@ -1,13 +1,7 @@
 import { z } from 'zod';
 
-export const TradeRequestStatusSchema = z.enum([
-  'PENDING',
-  'ACCEPTED',
-  'DECLINED',
-]);
+export const TradeRequestStatusSchema = z.enum(['PENDING','ACCEPTED','DECLINED']);
 
-export type TradeRequestStatusType = `${z.infer<
-  typeof TradeRequestStatusSchema
->}`;
+export type TradeRequestStatusType = `${z.infer<typeof TradeRequestStatusSchema>}`
 
 export default TradeRequestStatusSchema;
--- packages/types/generated/modelSchema/TradeOfferSchema.ts
+++ packages/types/generated/modelSchema/TradeOfferSchema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
-import { TradeOfferTypeSchema } from '../inputTypeSchemas/TradeOfferTypeSchema';
-import { TradeOfferStatusSchema } from '../inputTypeSchemas/TradeOfferStatusSchema';
+import { TradeOfferTypeSchema } from '../inputTypeSchemas/TradeOfferTypeSchema'
+import { TradeOfferStatusSchema } from '../inputTypeSchemas/TradeOfferStatusSchema'
 
 /////////////////////////////////////////
 // TRADE OFFER SCHEMA
@@ -17,30 +17,28 @@ export const TradeOfferSchema = z.object({
   message: z.string().nullable(),
   wantedComicId: z.number().int().nullable(),
   createdAt: z.coerce.date(),
-});
+})
 
-export type TradeOffer = z.infer<typeof TradeOfferSchema>;
+export type TradeOffer = z.infer<typeof TradeOfferSchema>
 
+/////////////////////////////////////////
 // TRADE OFFER PARTIAL SCHEMA
-//------------------------------------------------------
+/////////////////////////////////////////
 
-export const TradeOfferPartialSchema = TradeOfferSchema.partial();
+export const TradeOfferPartialSchema = TradeOfferSchema.partial()
 
-export type TradeOfferPartial = z.infer<typeof TradeOfferPartialSchema>;
+export type TradeOfferPartial = z.infer<typeof TradeOfferPartialSchema>
 
+/////////////////////////////////////////
 // TRADE OFFER OPTIONAL DEFAULTS SCHEMA
-//------------------------------------------------------
+/////////////////////////////////////////
 
-export const TradeOfferOptionalDefaultsSchema = TradeOfferSchema.merge(
-  z.object({
-    status: TradeOfferStatusSchema.optional(),
-    id: z.string().uuid().optional(),
-    createdAt: z.coerce.date().optional(),
-  }),
-);
+export const TradeOfferOptionalDefaultsSchema = TradeOfferSchema.merge(z.object({
+  status: TradeOfferStatusSchema.optional(),
+  id: z.string().uuid().optional(),
+  createdAt: z.coerce.date().optional(),
+}))
 
-export type TradeOfferOptionalDefaults = z.infer<
-  typeof TradeOfferOptionalDefaultsSchema
->;
+export type TradeOfferOptionalDefaults = z.infer<typeof TradeOfferOptionalDefaultsSchema>
 
 export default TradeOfferSchema;
