7c1a124414a34cc59db31c9a56c88c2521c524e8
pearlsky <pearlakpan115@gmail.com>
2023-04-14 15:43:08 +0100

    fix(prisma): run prisma generate to fix prisma errors, reimport prisma

--- packages/types/generated/inputTypeSchemas/TradeOfferScalarFieldEnumSchema.ts
+++ packages/types/generated/inputTypeSchemas/TradeOfferScalarFieldEnumSchema.ts
@@ -1,16 +1,5 @@
 import { z } from 'zod';
 
-export const TradeOfferScalarFieldEnumSchema = z.enum([
-  'id',
-  'type',
-  'status',
-  'createdById',
-  'price',
-  'phoneNumber',
-  'email',
-  'message',
-  'wantedComicId',
-  'createdAt',
-]);
+export const TradeOfferScalarFieldEnumSchema = z.enum(['id','type','status','createdById','price','phoneNumber','email','message','wantedComicId','createdAt']);
 
 export default TradeOfferScalarFieldEnumSchema;
