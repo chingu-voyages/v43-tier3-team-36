357963ab3c0c58b9dc84c4528d8bb243e93b1a35
Danthesage01 <ayenibabajidedaniel@gmail.com>
2023-04-16 16:57:23 +0100

    feat(modal): add modal for logout and edit profile page

--- packages/frontend/src/layouts/AuthLayout.tsx
+++ packages/frontend/src/layouts/AuthLayout.tsx
@@ -10,6 +10,8 @@ import { Avatar } from '@/components/ui';
 import { SEO, type SEOProps } from './SEO';
 import AlertPopup from '@/components/common/AlertPopup/AlertPopup';
 
+import { logout } from '@/api';
+
 type LayoutProps = {
   children: React.ReactNode;
   seo: SEOProps;
@@ -17,17 +19,47 @@ type LayoutProps = {
 
 const Navigation: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
+  const [showLogout, setShowLogout] = useState(false);
+
+  const handleLogout = () => {
+    logout();
+    setShowLogout(!showLogout);
+  };
 
   return (
-    <header className="flex items-center gap-x-2 py-4 px-3 border">
-      <Menu className="sm:hidden" onClick={() => setShowModal(true)} />
-      <div className="grow flex items-center justify-between">
-        <h1>Marvel Collector</h1>
-        <nav className="sm:grow sm:flex sm:items-center">
-          {/* Conditional rendering of nav menu for smaller devices */}
-          {showModal ? (
-            <div className="absolute top-0 left-0 z-50 w-screen h-screen bg-white">
-              <ul className="flex flex-col gap-y-8 py-14 px-8">
+    <>
+      <header className="flex items-center gap-x-2 py-4 px-3 border relative">
+        <Menu className="sm:hidden" onClick={() => setShowModal(true)} />
+        <div className="grow flex items-center justify-between">
+          <h1>Marvel Collector</h1>
+
+          <nav className="sm:grow sm:flex sm:items-center">
+            {/* Conditional rendering of nav menu for smaller devices */}
+            {showModal ? (
+              <div className="absolute top-0 left-0 z-50 w-screen h-screen bg-white">
+                <ul className="flex flex-col gap-y-8 py-14 px-8">
+                  <li>
+                    <Link href="/explore">Explore</Link>
+                  </li>
+                  <li>
+                    <Link href="/profile">Profile</Link>
+                  </li>
+                  <li>
+                    <Link href="/trades">Trades</Link>
+                  </li>
+                  <li>
+                    <Link href="/forums">Forums</Link>
+                  </li>
+                </ul>
+                <ArrowRight
+                  className="absolute top-4 right-6"
+                  onClick={() => setShowModal(false)}
+                />
+              </div>
+            ) : null}
+
+            <div className="grow hidden sm:block">
+              <ul className="flex items-center justify-center gap-x-8">
                 <li>
                   <Link href="/explore">Explore</Link>
                 </li>
@@ -35,48 +67,45 @@ const Navigation: React.FC = () => {
                   <Link href="/profile">Profile</Link>
                 </li>
                 <li>
-                  <Link href="/trades">Trades</Link>
-                </li>
-                <li>
                   <Link href="/forums">Forums</Link>
                 </li>
               </ul>
-              <ArrowRight
-                className="absolute top-4 right-6"
-                onClick={() => setShowModal(false)}
-              />
             </div>
-          ) : null}
-
-          <div className="grow hidden sm:block">
-            <ul className="flex items-center justify-center gap-x-8">
-              <li>
-                <Link href="/explore">Explore</Link>
-              </li>
-              <li>
-                <Link href="/profile">Profile</Link>
-              </li>
-              <li>
-                <Link href="/forums">Forums</Link>
-              </li>
-            </ul>
+            <div className="">
+              <div className="flex items-center gap-x-4 cursor-pointer">
+                {/* TODO: Add notification functionality */}
+                <Bell />
+                {/* TODO: Add saved bookmarks functionality */}
+                <Bookmark />
+                {/* TODO: Add user profile functionality */}
+                <Avatar
+                  src="https://placekeanu.com/250/g"
+                  alt="Keanu Reeves"
+                  size={30}
+                  onClick={() => setShowLogout(!showLogout)}
+                />
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+      {showLogout && (
+        <div className="bg-black rounded-md text-white absolute w-28 top-20 px-2 py-1 md:top-14 right-3">
+          <Link href="/login" className="text-base" onClick={handleLogout}>
+            Logout
+          </Link>
+          <div>
+            <Link
+              href="/edit-profile"
+              className="text-base"
+              onClick={() => setShowLogout(!showLogout)}
+            >
+              Edit Profile
+            </Link>
           </div>
-
-          <div className="flex items-center gap-x-4">
-            {/* TODO: Add notification functionality */}
-            <Bell />
-            {/* TODO: Add saved bookmarks functionality */}
-            <Bookmark />
-            {/* TODO: Add user profile functionality */}
-            <Avatar
-              src="https://placekeanu.com/250/g"
-              alt="Keanu Reeves"
-              size={30}
-            />
-          </div>
-        </nav>
-      </div>
-    </header>
+        </div>
+      )}
+    </>
   );
 };
 
