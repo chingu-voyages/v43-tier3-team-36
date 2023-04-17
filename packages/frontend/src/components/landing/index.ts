88d43be9ad543cc802816c3a16f9702d5977651c
pearlsky <pearlakpan115@gmail.com>
2023-04-17 12:25:15 +0100

    chore: write index file to serve as landing page components publicAPI

---
+++ packages/frontend/src/components/landing/index.ts
@@ -0,0 +1,11 @@
+import CTA from './CTA';
+import Features from './Feature';
+import Footer from './Footer';
+import Hero from './Hero';
+import Illustration from './Illustration';
+import Nav from './Nav';
+import Stats from './Stat';
+
+export {
+  Hero, Stats, Illustration, Features, CTA, Nav, Footer,
+};
